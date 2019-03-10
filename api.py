d3f01c29ad9b47eeda4e0ea8c52eb40d3087fb18
Chris Rowson <christopherrowson@gmail.com>
2019-03-10 17:48:00 +0000

    Added location
    Using the here.com reverse geolocation API grab street and city data to display as graph titles.

--- api.py
+++ api.py
@@ -3,6 +3,17 @@ import json
 import urllib.request
 
 
+def reverse_geocode(lat, lon):
+    appid = "grab code from here.com"
+    appcode = "grab code from here.com"
+    url = "https://reverse.geocoder.api.here.com/6.2/reversegeocode.json?prox=" \
+          + lat + "%2C" + lon + "%2C50&mode=retrieveAddresses&maxresults=1&gen=9&app_id=" \
+          + appid + "&app_code=" + appcode
+    data = urllib.request.urlopen(url)
+    location_details = json.loads(data.read())
+    return location_details
+
+
 def grab_sensor_list():
     list_devices = "https://aq.connectedhumber.org/api.php?action=list-devices&only-with-location=true"
     response = urllib.request.urlopen(list_devices)
@@ -30,7 +41,7 @@ def grab_sensor_urls():
 
 def grab_sensor_data():
     # iterate through the items in our dictionary and retrieve data from each url
-    # Create a new dict where the key is the sensor id and the value is the retreived json data
+    # Create a new dict where the key is the sensor id and the value is the retrieved json data
     sensor_reading_dict = {}
     url_dict = grab_sensor_urls()
     for key, value in url_dict.items():
@@ -51,6 +62,8 @@ def grab_sensor_data():
         sensor_list_details = []
         sensor_list_details.append(sensor_details)
         sensor_list_details.append(sensor_reading_dict.get(key))
+        sensor_location = reverse_geocode(str(sensor_details["latitude"]), str(sensor_details["longitude"]))
+        sensor_list_details.append(sensor_location)
         # update the sensor ID key with a list containing 2 values, sensor info and sensor data
         sensor_reading_dict.update({key: sensor_list_details})
     return sensor_reading_dict
